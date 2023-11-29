0a9cdf46ae11d8ea15c13d17f77886e9f433d97c
Alberto Morales <kodenix@gmail.com>
2023-11-29 19:25:46 +0100

    Added fields in user-register component

--- src/app/pages/user-register/user-register.component.ts
+++ src/app/pages/user-register/user-register.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'wta-user-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <p>
-      user-register works!
-    </p>
+    <form [formGroup]="registerForm" (submit)="onSubmit()">
+      <div>
+        <label for="name">Name</label>
+        <input formControlName="name" name="name" type="text"/>
+        <label for="email">Email</label>
+        <input formControlName="email" name="email"/>
+        <label for="password">Password</label>
+        <input formControlName="password" name="password" type="password"/>
+      </div>
+      <div>
+        <button type="submit" [disabled]="!registerForm.valid">Guardar</button>
+      </div>
+    </form>
   `,
-  styles: ``
+  styles: ``,
 })
 export class UserRegisterComponent {
+  private fb = inject(FormBuilder);
+  public registerForm = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
 }
